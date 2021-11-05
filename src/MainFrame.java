4ce69bd2821ad0fd594e65d198fe935bcc587111
Minirr <57798944+Minirr@users.noreply.github.com>
2021-11-06 02:29:09 +0900

    2021-11-06 2:29

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,24 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class MainFrame extends JFrame
 {
     public MainFrame()
     {
-        setTitle("도서 관리 시스템");
-        setSize(600, 800);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setVisible(true);
+        setTitle("도서관리 시스템");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setLocationRelativeTo(null);
+        setSize(600, 700);
         setResizable(false);
+        this.setLayout(null);
+
+        JButton logIn = new JButton("로그인");
+        logIn.setBounds(200, 460, 150, 40);
+        this.add(logIn);
+        JButton signUp = new JButton("회원가입");
+        signUp.setBounds(200, 530, 150, 40);
+        this.add(signUp);
     }
 }
